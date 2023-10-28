3cda73016fe7a7a2d16edd5c3c119fc56b6782a4
SnowHawkeye <hadriensalem@gmail.com>
2023-10-28 18:17:15 +0200

    adds functions to initialize the storage system

--- commands/timers/timers.js
+++ commands/timers/timers.js
@@ -7,7 +7,7 @@ module.exports = {
     .setDescription("Displays all in-progress timers."),
   async execute(interaction) {
 
-    const data = dataManager.getTimers(pathToStorage = "./data/storage")
+    const data = dataManager.getTimers()
     message = ''
 
     if (data.timers.length > 0) {
@@ -17,6 +17,6 @@ module.exports = {
         message += `\n> - ${timer.note}: Finishes <t:${timer.unix_end_time}:F>, which is <t:${timer.unix_end_time}:R>`
       }
     }
-    await interaction.reply({content: message, ephemeral: true});
+    await interaction.reply({ content: message, ephemeral: true });
   },
 };
--- data/dataManager.js
+++ data/dataManager.js
@@ -1,9 +1,41 @@
 const fs = require("fs");
+const path = require('path');
+const STORAGE_PATH = process.env.STORAGE_PATH;
+const TIMERS_PATH = path.join(STORAGE_PATH, "timers.json")
 
-function getTimers(pathToStorage) {
-    datString = fs.readFileSync(pathToStorage + "/timers.json");
-    return JSON.parse(datString);
+
+function initializeStorage() {
+    // Check whether the storage folder exists
+    if (!fs.existsSync(STORAGE_PATH)) {
+        console.log(`Storage directory created at ${STORAGE_PATH}.`)
+        fs.mkdirSync(STORAGE_PATH, { recursive: true }); // the `recursive` parameter lets the function work in case the folder is nested
+    }
+
+    // We have now ensured the storage folder exists, we can add individual storage files
+    // TODO Remove the dummy timer when actual timers can be created via the /send command (replace with empty array)
+    const dummyTimerObject = {
+        "timers": [
+            {
+                "unix_end_time": "1698532200",
+                "note": "Crapaud"
+            },
+            {
+                "unix_end_time": "1698532200",
+                "note": "Rainette"
+            }
+        ]
+    }
+    dummyTimer = JSON.stringify(dummyTimerObject)
+
+    if (!fs.existsSync(TIMERS_PATH)) {
+        fs.writeFileSync(TIMERS_PATH, dummyTimer, "UTF8")
+    }
 }
 
+function getTimers() {
+    console.log(`Retrieving timers from ${TIMERS_PATH}.`)
+    dataString = fs.readFileSync(TIMERS_PATH);
+    return JSON.parse(dataString);
+}
 
-module.exports = { getTimers } 
+module.exports = { getTimers, initializeStorage } 
