a49374a672b57003597f889b4d8633225a77a555
Julia Rose Chin <juliarosechin@gmail.com>
2023-10-30 02:35:59 -0700

    ensures instances of send are now set

--- commands/meta/info.js
+++ commands/meta/info.js
@@ -12,7 +12,7 @@ module.exports = {
 
 This is a bot that we created in order to better manage our submarines with a team of people. The original goal is to ping a specific person (or group of people) when submarines are ready to be resent on a voyage.
 
-For now, there are only two commands, \`send\` and \`timers\`. We will add the ability to register submarines and store information about them in a future update!
+For now, there are only two commands, \`set\` and \`timers\`. We will add the ability to register submarines and store information about them in a future update!
 
 ### For more detailed information about this bot, please [check out our GitHub repository](https://github.com/mu-ns/ffxiv-subs-bot)!
     `;
