2a50ab68c159bb79aa4e862212cbe3af13f426d1
Julia Rose Chin <juliarosechin@gmail.com>
2023-10-30 02:23:34 -0700

    implements cron job

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 // Require the necessary discord.js classes
 const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+
+// Other Imports
 require("dotenv").config();
 const token = process.env.DISCORD_TOKEN;
 const fs = require("node:fs");
 const path = require("node:path");
+const { CronJob } = require("cron");
+const { updateTimers } = require("./data/timerManager.js");
+const dataManager = require("./data/dataManager.js"); // storage system
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // Initialize the storage system
-const dataManager = require('./data/dataManager.js')
-dataManager.initializeStorage()
+dataManager.initializeStorage();
 
 // Retrieve commands from the command folder
 client.commands = new Collection();
@@ -37,6 +41,55 @@ for (const folder of commandFolders) {
 // Log that the bot is ready for use
 client.once(Events.ClientReady, () => {
   console.log("Ready!");
+
+  // Start cron job for timers
+  const job = new CronJob(
+    "* * * * *", // cronTime (every minute)
+    async function () {
+      var now = new Date();
+      console.log(`(${now.toString()}) Checking for timers... `);
+
+      const toSend = updateTimers();
+
+      // Sends messages if timers went off
+      if (toSend.length > 0) {
+        var index = 0;
+
+        while (index < toSend.length) {
+          var currTimer = toSend[index];
+          var note = currTimer.note == "" ? "Unnamed timer" : currTimer.note;
+          var msg = await `
+@everyone
+## Your submarine(s) have returned!
+
+> - ${note}`;
+
+          index++;
+
+          // Checks for additional timers from same channel
+          while (
+            index < toSend.length &&
+            toSend[index].channel_id == currTimer.channel_id
+          ) {
+            currTimer = toSend[index];
+            note = currTimer.note == "" ? "Unnamed timer" : currTimer.note;
+            msg += await `\n> - ${note}`;
+
+            index++;
+          }
+
+          console.log("Finding channel...");
+          channel = await client.channels.cache.get(currTimer.channel_id);
+          console.log("Sending message...");
+          channel.send(msg);
+        }
+      }
+
+      console.log("Done checking for timers!");
+    }, // onTick
+    null, // onComplete
+    true // start
+  );
 });
 
 // Start listening to events
